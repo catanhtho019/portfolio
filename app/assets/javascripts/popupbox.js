7a5a0666399cc981d420eced6501a9019ba0deda
Catherine Nguyen Cat <catanhtho@gmail.com>
2019-10-28 19:30:58 +0000

    homepage banner update

---
+++ app/assets/javascripts/popupbox.js
@@ -0,0 +1,38 @@
+const popup = (content) => {
+  const modal01 = document.getElementById("myModal01");
+  document.getElementById("myBtn01")
+  modal01.style.display = "block";
+};
+const closePopup = (content) => {
+  document.querySelector(".close01");
+  const modal01 = document.getElementById("myModal01");
+  modal01.style.display = "none";
+};
+
+// window.onclick = (event) => {
+//   if (event.target == modal01) {
+//     modal01.style.display = "none";
+//   }
+// };
+
+const popup02 = (content) => {
+  const modal02 = document.getElementById("myModal02");
+  document.getElementById("myBtn02");
+  modal02.style.display = "block";
+};
+const closePopup02 = (content) => {
+  document.querySelector(".close02");
+  const modal02 = document.getElementById("myModal02");
+  modal02.style.display = "none";
+};
+
+const popup03 = (content) => {
+  const modal03 = document.getElementById("myModal03");
+  document.getElementById("myBtn03")
+  modal03.style.display = "block";
+};
+const closePopup03 = (content) => {
+  document.querySelector(".close03");
+  const modal03 = document.getElementById("myModal03");
+  modal03.style.display = "none";
+};
---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,10 @@
+      function scrollDown() {
+      const aboutBtn = document.querySelector(".contact");
+      aboutBtn.scrollIntoView({behavior: "smooth"});
+    };
+
+
+    function scrollUp() {
+      const topBtn = document.querySelector("html");
+      topBtn.scrollIntoView({behavior: "smooth"});
+    };

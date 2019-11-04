6f404bbf4ee5cf53732ed12095da0294f4beae25
Catherine Nguyen Cat <catanhtho@gmail.com>
2019-11-04 22:44:07 +0000

    update controller

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,3 @@
 class HomepageController < ApplicationController
-  def home
-    # @project = Project.new
-    # @projects = Project.first(3)
-  end
-  def download_pdf
-  send_file "#{Rails.root}/app/assets/docs/Catherine_Nguyen-Cat_CV.pdf", type: "application/pdf", x_sendfile: true
-end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: :home
-  def project
-  end
 
-  def experiment
-  end
-
-  def about
-  end
 end

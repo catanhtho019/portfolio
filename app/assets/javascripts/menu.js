59971226f09b0c6be317c771f76e79b23efd983b
Catherine Nguyen Cat <catanhtho@gmail.com>
2019-11-29 02:19:35 +0000

    menu fix

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -19,3 +19,15 @@
 // });
 
 
+// const navSlide = () => {
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+
+//   burger.addEventListener('click',() => {
+//     nav.classList.toggle('.nav-active');
+//   });
+// }
+
+// navSlide();
+
+
